4c0f22134d32d332bef90f93815c88c74249a7ba
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-01-29 20:49:19 +0100

    auth storage completed

--- test/auth-storage-test.js
+++ test/auth-storage-test.js
@@ -1,17 +1,45 @@
 "use strict";
 
 var expect = require("expect.js"),
+    fs = require("fs"),
     AuthStorage = require("../lib/auth-storage");
 
 
 describe("AuthStorage", function() {
-    var authStorage;
+    var originalConfigFile = "test/user-storage/original-users.json",
+        configFile = "test/user-storage/users.json",
+        authStorage;
 
     before(function(done) {
-        authStorage = new AuthStorage({
-            file: "test/user-storage/users.json",
-            onReady: done
-        });
+
+        function createIt() {
+            authStorage = new AuthStorage({
+                file: configFile,
+                onReady: done
+            });
+        }
+
+        if (! fs.existsSync(configFile)) {
+            var wr = fs.createWriteStream(configFile),
+                rd = fs.createReadStream(originalConfigFile);
+
+            rd.on("error", function(err) {
+                throw err;
+            });
+
+            wr.on("error", function(err) {
+                throw err;
+            });
+
+            wr.on("close", createIt);
+
+
+            rd.pipe(wr);
+
+        } else {
+            createIt();
+        }
+
     });
 
     it("is defined", function() {
@@ -27,7 +55,7 @@ describe("AuthStorage", function() {
             expect(usr).to.be.an("object");
             expect(usr.fullname).to.be.equal("parroit");
             done();
-        })
+        });
 
 
 
@@ -35,11 +63,12 @@ describe("AuthStorage", function() {
 
     function itInvokeStorageSave(action) {
         describe("cause storageSaved event to be emitted", function() {
+            var configExists = false;
             before(function(done) {
                 var count = 0;
 
                 function checkStorageSaved() {
-
+                    configExists = fs.existsSync(configFile);
                     done();
                     authStorage.events.removeListener("storageSaved", checkStorageSaved);
                 }
@@ -48,6 +77,10 @@ describe("AuthStorage", function() {
                     if (!authStorage.dirty) {
 
                         authStorage.events.on("storageSaved", checkStorageSaved);
+
+                        if (fs.existsSync(configFile)) {
+                            fs.unlinkSync(configFile);
+                        }
 
                         action();
 
@@ -64,6 +97,10 @@ describe("AuthStorage", function() {
 
             it("set dirty to false", function() {
                 expect(authStorage.dirty).to.be.equal(false);
+            });
+
+            it("save users to file", function() {
+                expect(configExists).to.be.equal(true);
             });
         });
     }
