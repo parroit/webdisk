b7d30f44f00379c02729b8b95cfc3555046724c7
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-01-30 21:56:24 +0100

    selection

--- client/js/files.js
+++ client/js/files.js
@@ -1,14 +1,47 @@
-
-
 (function model(exports, global) {
 	var filesModel = {
 		events: new EventEmitter(),
+		files: [],
+		toggleFileSelection: toggleFileSelection,
+		selection: selection,
 		readFiles: readFiles
 	};
 
+	function selection() {
+		function isSelected(f) {
+			return f.selected;
+		}
+
+		return filesModel.files.filter(isSelected);
+
+	}
+
+
+	function toggleFileSelection(filePath) {
+		function matchPath(f) {
+			return f.path === filePath;
+		}
+
+		var file = filesModel.files.filter(matchPath)[0];
+		file.selected = !file.selected;
+
+		var selectionLen = selection().length;
+
+		if (selectionLen === 1) {
+
+			filesModel.events.emit("selectionNotEmpty");
+
+		} else if (selectionLen === 0) {
+
+			filesModel.events.emit("selectionEmpty");
+
+		}
+
+	}
+
 	function readFiles(folder) {
 		var url = "files/" + encodeURIComponent(folder);
-		
+
 		$.get(url)
 
 		.fail(onFailure)
@@ -16,16 +49,17 @@
 		.done(function(files) {
 			filesModel.files = files;
 			filesModel.events.emit("filesChanged");
-			
+
 		});
 	}
-	
+
 	function onFailure(xhr, textStatus, error) {
 		console.log(textStatus + ": " + error);
 	}
 
 	exports.files = {
 		model: filesModel
+
 	};
 
 	readFiles("parroit");
@@ -34,10 +68,13 @@
 
 
 (function view(exports, global) {
-	var $ = global.jQuery;
+	var $ = global.jQuery,
+		filesModel = global.files.model,
+		selectionButtons = $("#download-btn, #remove-btn, #move-btn, #rename-btn");
 
 	function addFile(file) {
 		var body = $("#files tbody"),
+			pathEncoded = encodeURIComponent(file.path).replace(/\'/g, "&apos;")
 
 			tr = $(
 				"<tr>" +
@@ -46,9 +83,13 @@
 				"	<td class='uploaded'/>" +
 				"	<td class='actions'/>" +
 				"</tr>"
-			);
+			),
 
-		var $name = tr.find(".name");
+
+			$name = tr.find(".name");
+
+		tr.attr("id", file.path);
+
 		$name.append(
 			"<img src='/img/" + file.icon + "''>" +
 			"&nbsp;<span>" + file.name + "</span>"
@@ -57,8 +98,6 @@
 		tr.find(".size").text(file.size);
 		tr.find(".uploaded").text(file.uploaded);
 
-		var pathEncoded = encodeURIComponent(file.path).replace(/\'/g, "&apos;");
-		console.log(pathEncoded);
 
 		tr.find(".actions").html(
 			"<a " +
@@ -75,16 +114,38 @@
 
 	}
 
-	
+	function registerFilesHandlers() {
+		$("tr").click(function() {
+			var path = $(this).attr("id");
 
-	
+			$(this).toggleClass("info");
+			filesModel.toggleFileSelection(path);
+		});
+	}
 
-	global.files.model.events.on("filesChanged",function(){
+
+
+	filesModel.events.on("filesChanged", function() {
 		$("#files tbody").html("");
 		global.files.model.files.forEach(addFile);
+		registerFilesHandlers();
 	});
 
-	
+
+
+	filesModel.events.on("selectionNotEmpty", function() {
+		selectionButtons.removeAttr("disabled");
+	});
+
+
+
+	filesModel.events.on("selectionEmpty", function() {
+		selectionButtons.attr("disabled","");
+	});
+
+
+
+	//exports.files.view = filesView;
 
 
 })(window, window);
